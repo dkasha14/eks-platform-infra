c0e36b2a72d4bcabac9ff602e72cb068939abaf6
dkasha14 <dlrashadk@gmail.com>
2026-03-28 07:44:53 +0000

    Configure remote state backend for SIT environment using S3 and DynamoDB

---
+++ environments/sit/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "asha-eks-cluster-tf-state"
+    key            = "sit/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "asha-eks-terraform-locks"
+    encrypt        = true
+  }
+}

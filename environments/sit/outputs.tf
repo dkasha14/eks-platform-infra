7198e16037fde8dad374689e2c2067ff705cecc0
dkasha14 <dlrashadk@gmail.com>
2026-03-28 07:34:09 +0000

    Expose SIT environment outputs including autoscaler IAM role for integration

---
+++ environments/sit/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
